1d0b98516cb6385001f9c52721023a3fb6c28958
Gemar Johnston <brazil.johnston@gmail.com>
2019-01-29 16:01:32 -0500

    Add files via upload
    Updates to Broship!2019

---
+++ Broship!2019.js
@@ -0,0 +1,13 @@
+alert("This is crazy!");
+prompt("Help");
+
+let ball= "dog";
+console.log(ball);
+let cat= prompt("Cat is here");
+cat;
+console.log(cat);
+
+let boat= "Ship";
+console.log(boat);
+let bird= prompt("Bird fly!");
+console.log();
