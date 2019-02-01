8e27355905854b12fda0382efce3aa590854d88f
Gemar Johnston <brazil.johnston@gmail.com>
2019-02-01 16:46:50 -0500

    Add files via upload
    This is an update to the Hangman!2019 project.

--- Hangman!2019.js
+++ Hangman!2019.js
@@ -14,90 +14,94 @@ let words = [
   "Master",
   "python"
 ];
+const newLocal = words[Math.random() * words.length];
 
-const newLocal = words[Math.floor(Math.random() * words.length)];
+console.log("Dog, fish, cat");(Math.random() * words.length);
+console.log(words[0]);(Math.random)
+console.log(words[Math.floor(Math.random()*13)]);
 
-// This is  the amswer answerArrayvar answerArray = [];
-for (let i = 0; i < word.length; i++) {
-  answerArray[i] = "WAT";
-}
 
-let remainingLetters = word.length;
+// // This is  the amswer answerArrayvar answerArray = [];
+// for (let i = 0; i < word.length; i++) {
+//   answerArray[i] = "";
+// }
 
-// This section is a  game loop
+// let remainingLetters = word.length;
 
-while (remainingLetters > 0) {
-  // Show player progress alert(answerArray.join(" "));
+// // This section is a  game loop
 
-  // Get a guess from the player
+// while (remainingLetters > 0) {
+//   // Show player progress alert(answerArray.join(" "));
 
-  let guess = prompt("Guess a letter, or click Cancel to stop playing.");
-  if (guess === null) {
-    // Exit the game loop
-    break;
-  } else if (guess.length !== 1) {
-    alert("Please enter a single letter dude.");
-  } else {
-    // Update the game state with the guess
+//   // Get a guess from the player
 
-    for (let j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
-        answerArray[j] = guess;
-        remainingLetters--;
-      }
-    }
-  }
+//   let guess = prompt("Guess a letter, or click Cancel to stop playing.");
+//   if (guess === null) {
+//     // Exit the game loop
+//     break;
+//   } else if (guess.length !== 1) {
+//     alert("Please enter a single letter dude.");
+//   } else {
+//     // Update the game state with the guess
 
-  // The end of the game loop
-}
+//     for (let j = 0; j < word.length; j++) {
+//       if (word[j] === guess) {
+//         answerArray[j] = guess;
+//         remainingLetters--;
+//       }
+//     }
+//   }
 
-// Show the answer and you did it!
+//   // The end of the game loop
+// }
 
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
+// // Show the answer and you did it!
 
-// A list of the funtions used.
+// alert(answerArray.join(" "));
+// alert("Good job! The answer was " + word);
 
-let word = pickWord();
-let answerArray = setupAnswerArray(word);
-let remainingLetters = word.length;
-let pickWord = function () {
-  // Return a random word
-};
+// // A list of the funtions used.
 
-let setupAnswerArray = function (word) {
-  // Return the answer array
-};
+// let word = pickWord();
+// let answerArray = setupAnswerArray(word);
+// let remainingLetters = word.length;
+// let pickWord = function () {
+//   // Return a random word
+// };
 
-let showPlayerProgress = functin(answerArray); {
-  //Use alert to show the player their progress
-};
+// let setupAnswerArray = function (word) {
+//   // Return the answer array
+// };
 
-let getGuess = function () {
-  // Use prompt to get a guess
-};
+// let showPlayerProgress = functin(answerArray); {
+//   //Use alert to show the player their progress
+// };
 
-let updateGameState = function (guess, word, answerArray) {
-  // Update answerArray and return a number showing how many times the guess appears in the word so remainingLetters can be updated
-};
+// let getGuess = function () {
+//   // Use prompt to get a guess
+// };
 
-let showAnswerAndCongratulatePlayer = function (answerArray) {
-  // Use alert to show the answer and tell the player well done.
-};
-while (remainingLetters > 0) {
-  showPlayerProgress(answerArray);
-  let guess = getGuess();
-  if (guess === null) {
-    break;
-  } else if (guess.length !== 1) {
-    alert("Please enter a single letter.");
-  } else {
-    let correctGuesses = updateGameState(guess, word, answerArray);
-    remainingLetters -= correctGuesses;
-  }
-}
+// let updateGameState = function (guess, word, answerArray) {
+//   // Update answerArray and return a number showing how many times the guess appears in the word so remainingLetters can be updated
+// };
 
-showAnswerAndCongratulatePlayer(answerArray);
+// let showAnswerAndCongratulatePlayer = function (answerArray) {
+//   // Use alert to show the answer and tell the player well done.
+// };
+// while (remainingLetters > 0) {
+//   showPlayerProgress(answerArray);
+//   let guess = getGuess();
+//   if (guess === null) {
+//     break;
+//   } else if (guess.length !== 1) {
+//     alert("Please enter a single letter.");
+//   } else {
+//     let correctGuesses = updateGameState(guess, word, answerArray);
+//     remainingLetters -= correctGuesses;
+//   }
+// }
+
+// showAnswerAndCongratulatePlayer(answerArray);
 
 
 
