6be57ffc49c0457d0cb4d579365c1cbea4684ec3
Gemar Johnston <brazil.johnston@gmail.com>
2019-02-11 15:59:59 -0500

    Add files via upload
    First week work for PHP. Will be updated.

---
+++ PHPwor.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Make this work.</h1>
+    <?php
+    <b> Here is the html</b>
+    ?>
+</body>
+</html>
+
+<?php
+//This project is declaring variables.
+$datBoi = "A big ol thing";
+
+echo $datBoi
+
+$a = "Get dat money";
+$b = $a . "Money!";
+
+echo $a
