3d1990ea9453bcbc02809f47f7ebfcb0b5610501
Gemar Johnston <brazil.johnston@gmail.com>
2019-02-12 15:38:41 -0500

    Add files via upload
    Commit changes to the following files for daily backup.

---
+++ LoopthroghArrayIF.php
@@ -0,0 +1,26 @@
+<?php
+
+$Ship = array("NX01", "NC101", "XN88", "NA92", "R");
+echo "Star" .  $Ship[0] . ", " . $Ship[1] . " and " . $Ship[2] . ".";
+$Row = 0;
+
+while($Row <= 4){
+if (strlen($Ship[$Row]) < 4) {
+    echo "Warp 7.2";
+for ($Ship = 0 < 5; $Ship++){
+    $Row = 10 * $Ship;
+    echo "Warp 9.5 os 5 * $Ship"
+}
+    for ($Row = 0; $Row < 0; $Row++) {
+    echo $Row;
+    
+}
+    
+}
+
+echo "<br>";
+echo $Ship[$Row];
+$Row++;
+}
+
+?>
