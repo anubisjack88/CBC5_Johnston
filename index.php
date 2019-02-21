4d2106c96e571e4a60c676c9ee91c00786c6216d
Gemar Johnston <brazil.johnston@gmail.com>
2019-02-21 12:02:50 -0500

    Add files via upload
    This is an update for my first app.

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
+<form action = "./add.php"method ="post"> 
+<input type = "text" name = "task">
+<input type = "submit">
+</form>
 
-CREATE TABLE Persons
-(
-PersonID int,
-LastName varchar(255),
-FirstName varchar(255),
-Address varchar(255),
-City archer(255)
-);
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todo";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, task FROM mytask"); 
+    $stmt->execute();
+
+    
+    $results = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    $results = $stmt->fetchAll();
+    
+    
+    foreach($results as $result){
+?>
+        <li><?php echo $result["task"]; ?></li>
+        <form action = "./delete.php"method ="post"> 
+        <input type = "text" name = "id" value = <?php echo $result["id"];?> hidden >
+        <input type = "submit">
+    
+        </form>
+
+ <?php
+    }
+    
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+
+
+?>
+
+
